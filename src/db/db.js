84744b47466164de458080f06d30f8a4a5251112
nmhung2022 <nmhung2022@gmail.com>
2021-08-06 23:45:02 +0700

    Update data db.json

--- src/db/db.js
+++ src/db/db.js
@@ -40,3 +40,22 @@ export const ExploreData = [
     distance: '2 hour drive',
   },
 ];
+
+export const LiveAnywhereData = [
+  {
+    img: 'https://a0.muscache.com/im/pictures/2f13349d-879d-43c6-83e3-8e5679291d53.jpg?im_w=1440',
+    title: 'Outdoor getaways',
+  },
+  {
+    img: 'https://a0.muscache.com/im/pictures/36f53e61-db8d-403c-9122-5b761c0e4264.jpg?im_w=1440',
+    title: 'Unique stays',
+  },
+  {
+    img: 'https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=1440',
+    title: 'Entire homes',
+  },
+  {
+    img: 'https://a0.muscache.com/im/pictures/10a638e1-6aff-4313-8033-1275cec83987.jpg?im_w=1440',
+    title: 'Pet allowed',
+  },
+];
