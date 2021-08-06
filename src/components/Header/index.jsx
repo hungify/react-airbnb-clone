bc0c4e4591e76a8fb103438b70fb4c751e018293
nmhung2022 <nmhung2022@gmail.com>
2021-08-06 12:21:13 +0700

    ♻️

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import headerStyle from './Header.module.css';
 import {
@@ -8,10 +8,28 @@ import {
   UserIcon,
   UserCircleIcon,
 } from '@heroicons/react/solid';
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 Header.propTypes = {};
 
 function Header(props) {
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
   return (
     <header className={headerStyle.inner}>
       <div className={headerStyle.wrapper__logo}>
@@ -23,7 +41,13 @@ function Header(props) {
       </div>
 
       <div className={headerStyle.wrapper__search}>
-        <input className={headerStyle.input__search} type="text" placeholder="Start your search" />
+        <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          className={headerStyle.input__search}
+          type="text"
+          placeholder="Start your search"
+        />
         <SearchIcon className={headerStyle.icon__search} />
       </div>
 
@@ -35,6 +59,18 @@ function Header(props) {
           <UserCircleIcon className={headerStyle.user__icon} />
         </div>
       </div>
+
+      {searchInput && (
+        <div className={headerStyle.wrapper__date}>
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#fd5b61']}
+            onChange={handleSelect}
+            // dateDisplayFormat="dd/MM/yyyy"s
+          />
+        </div>
+      )}
     </header>
   );
 }
