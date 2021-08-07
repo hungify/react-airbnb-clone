31d8592fbb67fa6537d282be7c8596e85b613b28
nmhung2022 <nmhung2022@gmail.com>
2021-08-08 00:32:45 +0700

    Update components

--- src/components/LargeCard/index.jsx
+++ src/components/LargeCard/index.jsx
@@ -8,15 +8,15 @@ function LargeCard({ img, title, description, buttonText }) {
   return (
     <section className={styled.hero__inner}>
       <div className={styled.wrapper__img}>
-        <div>
+        <div> 
           <img src={img} alt={title} className={styled.img} />
         </div>
-      </div>
-      <div className={styled.wrapper__text}>
-        <h3 className={styled.title}>{title}</h3>
-        <p className={styled.desc}>{description}</p>
+        <div className={styled.wrapper__text}>
+          <h3 className={styled.title}>{title}</h3>
+          <p className={styled.desc}>{description}</p>
 
-        <button className={styled.btn}>{buttonText}</button>
+          <button className={styled.btn}>{buttonText}</button>
+        </div>
       </div>
     </section>
   );
--- src/components/MediumCard/index.jsx
+++ src/components/MediumCard/index.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from './MediumCard.module.css';
 
-MediumCard.propTypes = {};
+MediumCard.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string,
+};
 
 function MediumCard({ img, title }) {
   return (
     <div className={styled.inner}>
       <div className={styled.wrapper__img}>
-        <img className={styled.img} src={img} alt={title} />
+        <div>
+          <img className={styled.img} src={img} alt={title} />
+        </div>
       </div>
       <h3 className={styled.title}>{title}</h3>
     </div>
