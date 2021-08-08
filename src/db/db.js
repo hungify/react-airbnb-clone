be4603b335993419455c93c676f99e15d5d60fe2
nmhung2022 <nmhung2022@gmail.com>
2021-08-08 23:26:50 +0700

    Update data

--- src/db/db.js
+++ src/db/db.js
@@ -59,3 +59,22 @@ export const LiveAnywhereData = [
     title: 'Pet allowed',
   },
 ];
+
+export const DiscoverData = [
+  {
+    title: 'Featured collection: Wanderlust',
+    description: 'Travel from home with Online Experiences.',
+    img: 'https://a0.muscache.com/im/pictures/247a1ea3-946d-4eb8-a6ab-e8b9a66846f4.jpg?im_w=1200',
+  },
+  {
+    title: 'Online Experiences',
+    description: 'Live, interactive activities led by Hosts.',
+    img: 'https://a0.muscache.com/im/pictures/a6b08861-feb8-4a01-a76d-b33d2867b441.jpg?im_w=1200',
+  },
+
+  {
+    title: 'Experiences',
+    description: 'Find unforgettable activities near you.',
+    img: 'https://a0.muscache.com/im/pictures/ad109d56-2421-40cd-98e6-e114160dc85b.jpg?im_w=1200',
+  },
+];
